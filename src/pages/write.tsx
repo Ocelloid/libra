8b6b7f4251957b4018ac850e1fe48cf40daadc0d
Nikita Grebnev <n.a.grebnev@gmail.com>
2024-03-08 00:06:52 +0500

    adding theme change

--- src/pages/write.tsx
+++ src/pages/write.tsx
@@ -1,4 +1,4 @@
-import { Button, Slider } from "@nextui-org/react";
+import { Button, Slider, Tooltip } from "@nextui-org/react";
 import { useSession } from "next-auth/react";
 import Head from "next/head"
 import { useRouter } from "next/router";
@@ -11,9 +11,10 @@ const Write = () => {
     const { data: sessionData } = useSession();
     const { replace } = useRouter();
 
-    const [entryContent, setEntryContent] = useState("");
-    const [entryTitle,   setEntryTitle]   = useState("");
-    const [weightRating, setWeightRating] = useState(50);
+    const [entryContent, setEntryContent] = useState<string>("");
+    const [entryTitle,   setEntryTitle  ] = useState<string>("");
+    const [weightRating, setWeightRating] = useState<number>(50);
+    const [inputValue,   setInputValue  ] = useState<string>("50");
 
     const { mutate: createEntry, isLoading } = api.weightedEntry.createEntry.useMutation({
         onSuccess(data) {
@@ -52,7 +53,7 @@ const Write = () => {
             >
                 <section className="sec-container">
                     <div className="mx-auto flex md:w-3/4 lg:w-2/3 xl:w-1/2 2xl:w-3/7 flex-col gap-5">
-                        <form className="flex w-full flex-col justify-center gap-5" onSubmit={e => handleFormSubmit(e)}>
+                        <form className="flex w-full flex-col justify-center gap-5" onSubmit={e => handleFormSubmit(e)} onKeyPress={(e) => { e.key === 'Enter' && e.preventDefault(); }}>
                             <input 
                                 required
                                 value={entryTitle}
@@ -75,7 +76,7 @@ const Write = () => {
                                 step={1} 
                                 minValue={0} 
                                 maxValue={100}                                  
-                                aria-label="Вес"
+                                label="Вес:&nbsp;"
                                 showTooltip={true}
                                 value={weightRating}
                                 onChange={value => setWeightRating(Number(value))}
@@ -85,7 +86,7 @@ const Write = () => {
                                     radius="full"
                                     variant="light"
                                     onPress={() => setWeightRating(0)}>
-                                    0
+                                        0
                                     </Button>
                                 }
                                 endContent={
@@ -93,11 +94,40 @@ const Write = () => {
                                     isIconOnly
                                     radius="full"
                                     variant="light"
-                                    onPress={() => setWeightRating(100)}
-                                    >
-                                    100
+                                    onPress={() => setWeightRating(100)}>
+                                        100
                                     </Button>
-                                }
+                                }    
+                                renderValue={(...props) => (
+                                    <output {...props}>
+                                        <Tooltip
+                                            className="text-tiny text-default-500 rounded-md"
+                                            content="Нажми Enter чтобы применить"
+                                            placement="left">
+                                        <input
+                                            className="px-1 py-0.5 w-10 my-1 text-center text-medium font-montserrat text-gray-500 font-medium bg-slate-800 outline-none transition-colors rounded-small border-medium border-transparent hover:border-primary focus:border-primary bg-transparent"
+                                            type="text"
+                                            aria-label="Вес"
+                                            value={inputValue}
+                                            onChange={(e) => {
+                                                const v = e.target.value;
+                                                setInputValue(v);
+                                            }}
+                                            onKeyDown={(e) => {
+                                                if (e.key === "Enter" && !isNaN(Number(inputValue))) {
+                                                    setWeightRating(Number(inputValue));
+                                                }
+                                            }}/>
+                                        </Tooltip>
+                                    </output>
+                                )}    
+                                color="foreground"
+                                classNames={{
+                                    label: "ml-auto text-medium text-gray-500 font-montserrat",
+                                    value: "text-medium text-gray-500 font-montserrat",
+                                    filler: "bg-neutral-100 hover:bg-gray-300",
+                                    thumb: "bg-neutral-100 hover:bg-gray-300"
+                                }}
                                 className="mx-auto"/>
                             <button 
                                 type="submit" 
